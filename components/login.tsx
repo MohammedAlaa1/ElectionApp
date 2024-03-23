c40822c3b65d445fd4f540b4399f5a30da6052eb
MohammedAlaa1 <126056161+MohammedAlaa1@users.noreply.github.com>
2024-03-23 19:19:57 +0200

    .

--- components/login.tsx
+++ components/login.tsx
@@ -30,7 +30,7 @@ const LoginPage = () => {
             console.log("an error occurred: ", error);
         }
     }
-
+ 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8 bg-blue-50 rounded-lg shadow-lg p-8">
